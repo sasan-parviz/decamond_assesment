df527fc64e16aece08ddeabb4c73bddc627dda3a
Sasan Parviz <sasanparviz98@gmail.com>
2025-07-31 02:47:19 +0330

    feat(app): added layout.tsx with Auth Provider and global style

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import { AuthProvider } from "@/context/AuthContext";
+import "./globals.scss";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
